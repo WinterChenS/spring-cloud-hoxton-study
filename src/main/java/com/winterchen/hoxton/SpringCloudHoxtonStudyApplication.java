90ff34d28a5ab33daec886109d3a2c5a93193419
WinterChenS <1085143002@qq.com>
2021-07-22 13:55:18 +0800

    init

---
+++ src/main/java/com/winterchen/hoxton/SpringCloudHoxtonStudyApplication.java
@@ -0,0 +1,13 @@
+package com.winterchen.hoxton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudHoxtonStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudHoxtonStudyApplication.class, args);
+    }
+
+}
