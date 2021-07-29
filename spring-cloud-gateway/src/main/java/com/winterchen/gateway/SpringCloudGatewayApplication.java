d1ca37c0895c690895364e9e71c1c4549a276169
WinterChenS <1085143002@qq.com>
2021-07-29 10:15:08 +0800

    feature：新增gateway的使用

---
+++ spring-cloud-gateway/src/main/java/com/winterchen/gateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.winterchen.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-gateway/src/test/java/com/winterchen/gateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winterchen.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
